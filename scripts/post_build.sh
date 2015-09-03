de8488c56563b3974127889f460dd16976b63bf1
Jason Pruitt <jrspruitt@gmail.com>
2015-09-02 22:37:44 -0700

    Fixed post_image script to allow root ssh login

--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -4,3 +4,9 @@ BOARDS_PATH=$(dirname $(dirname $BASH_SOURCE))
 FILES=$BOARDS_PATH/files
 
 cp -rf $FILES/* $TARGET_DIR/
+
+SSHD_CONFIG=$TARGET_DIR/etc/ssh/sshd_config
+
+if [ -f $SSHD_CONFIG ]; then
+    sed -i 's/#PermitRootLogin no/PermitRootLogin yes/g' $SSHD_CONFIG
+fi
