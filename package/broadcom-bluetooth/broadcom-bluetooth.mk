410e4027a433666d03dc98bfb75d7524e2201d4a
Jason Pruitt <jrspruitt@gmail.com>
2015-09-02 21:44:15 -0700

    Updated bluetooth package github location and init script

--- package/broadcom-bluetooth/broadcom-bluetooth.mk
+++ package/broadcom-bluetooth/broadcom-bluetooth.mk
@@ -5,11 +5,11 @@
 ################################################################################
 
 BROADCOM_BLUETOOTH_VERSION = master
-BROADCOM_BLUETOOTH_SITE = $(call github,hmallat,broadcom-bluetooth,$(BROADCOM_BLUETOOTH_VERSION))
+BROADCOM_BLUETOOTH_SITE = $(call github,ARMWorks,broadcom-bluetooth,$(BROADCOM_BLUETOOTH_VERSION))
 BROADCOM_BLUETOOTH_LICENSE = APACHE-2.0
 
 define BROADCOM_BLUETOOTH_BUILD_CMDS
-	$(TARGET_CC) $(TARGET_CFLAGS) $(@D)/broadcom-bluetooth/brcm_patchram_plus.c -o $(@D)/brcm_patchram_plus
+	$(TARGET_CC) $(TARGET_CFLAGS) $(@D)/brcm_patchram_plus.c -o $(@D)/brcm_patchram_plus
 endef
 
 define BROADCOM_BLUETOOTH_INSTALL_TARGET_CMDS
