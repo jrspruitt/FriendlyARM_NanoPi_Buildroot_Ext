cee47e786029a188cac8b5383afc0989872dd99d
Jason Pruitt <jrspruitt@gmail.com>
2015-09-04 23:46:47 -0700

    Fixed fgpio demo make file

--- package/fgpio_demo/fgpio_demo.mk
+++ package/fgpio_demo/fgpio_demo.mk
@@ -8,17 +8,15 @@ FGPIO_DEMO_VERSION = master
 FGPIO_DEMO_SITE = $(call github,jrspruitt,FriendlyARM_NanoPi_Qt_GPIO_Demo,$(FGPIO_DEMO_VERSION))
 FGPIO_DEMO_LICENSE = MIT
 FGPIO_DEMO_LICENSE_FILES = LICENSE
+FGPIO_DEMO_DEPENDENCIES = qapplauncher
 
-ifdef BR2_PACKAGE_QAPPLAUNCHER
-QAPP_ADDER=python $(BR2_EXTERNAL)/package/qapplauncher/qapp_adder.py $(TARGET_DIR) /opt/QAppConfig/settings/default.ini /opt/fgpio_demo/run_demo.py
-endif
 
 define FGPIO_DEMO_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/opt/fgpio_demo
-	cp -dpfr $(@D)/logo.png $(TARGET_DIR)/opt/fgpio_demo/logo.png
+	cp -dpfr $(BR2_EXTERNAL)/package/fgpio_demo/logo.png $(TARGET_DIR)/opt/fgpio_demo/logo.png
 	cp -dpfr $(@D)/run_demo.py $(TARGET_DIR)/opt/fgpio_demo/run_demo.py
 	cp -dpfr $(@D)/demo_ui.py $(TARGET_DIR)/opt/fgpio_demo/demo_ui.py
-	$(QAPP_ADDER)
+	python $(BR2_EXTERNAL)/package/qapplauncher/qapp_adder.py $(TARGET_DIR) /opt/QAppConfig/settings/default.ini /opt/fgpio_demo/run_demo.py
 endef
 
 $(eval $(generic-package))
