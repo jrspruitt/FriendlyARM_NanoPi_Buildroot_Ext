a29f3a1eb178cc9eb1cbd953502d55a935ed893e
Jason Pruitt <jrspruitt@gmail.com>
2015-12-22 01:17:45 -0800

    Added NanoPi2

--- package/broadcom-bluetooth/broadcom-bluetooth.mk
+++ package/broadcom-bluetooth/broadcom-bluetooth.mk
@@ -16,8 +16,4 @@ define BROADCOM_BLUETOOTH_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/brcm_patchram_plus $(TARGET_DIR)/usr/bin/brcm_patchram_plus
 endef
 
-define BROADCOM_BLUETOOTH_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL)/package/broadcom-bluetooth/S60bluetooth $(TARGET_DIR)/etc/init.d/S60bluetooth
-endef
-
 $(eval $(generic-package))
--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-
+BOARD_NAME=$2
 BOARDS_PATH=$(dirname $(dirname $BASH_SOURCE))
-FILES=$BOARDS_PATH/files
+OVERLAY_PATH=$BOARDS_PATH/overlays
+COMMON_OVERLAY=$OVERLAY_PATH/common
+BOARD_OVERLAY=$OVERLAY_PATH/$BOARD_NAME
 
-cp -rf $FILES/* $TARGET_DIR/
+cp -rf $COMMON_OVERLAY/* $TARGET_DIR/
+cp -rf $BOARD_OVERLAY/* $TARGET_DIR/
 
+cp -r
 SSHD_CONFIG=$TARGET_DIR/etc/ssh/sshd_config
 
 if [ -f $SSHD_CONFIG ]; then
     sed -i 's/#PermitRootLogin no/PermitRootLogin yes/g' $SSHD_CONFIG
+fi
+
+AVAHID_CONFIG=$TARGET_DIR/etc/avahi/avahi-daemon.conf
+
+if [ -f $AVAHID_CONFIG ]; then
+    sed -i 's/#deny-interfaces=eth1/deny-interfaces=wlan0/g' $AVAHID_CONFIG
 fi
